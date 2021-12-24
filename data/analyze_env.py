7a307baf703024d172ab51bb78f5425cc086a98c
chenkq <sy2106101@buaa.edu.cn>
2021-12-25 00:28:02 +0800

    add env support in split_data

--- data/analyze_env.py
+++ data/analyze_env.py
@@ -1,13 +1,4 @@
-from collections import defaultdict
-
-import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-
-t_path = r"train.csv"
-e_path = r"train_extra.csv"
-train_df = pd.read_csv(t_path)
-extra_df = pd.read_csv(e_path)
 
 group2subgroups = {
     "disability": ["[no_disability]", "physical_disability", "psychiatric_or_mental_illness", "intellectual_or_learning_disability",
@@ -16,71 +7,95 @@ group2subgroups = {
     "sexual_orientation": ["[no_sexual]", "homosexual_gay_or_lesbian", "heterosexual", "bisexual", "other_sexual_orientation"],
     "race_or_ethnicity": ["[no_race]", "black", "white", "asian", "latino", "jewish", "other_race_or_ethnicity"],
     "religion": ["[no_religion]", "christian", "buddhist", "atheist", "muslim", "hindu", "other_religion"],
+    # "misc": ["disagree", "sad", "funny", "wow", "likes"],
 }
 
-# "misc": ["disagree", "sad", "funny", "wow", "likes"],
 
-subgroup2group = {}
-for g, subs in group2subgroups.items():
-    for sub in subs:
-        subgroup2group[sub] = g
-
-
-def pos2label(p):
-    if p > 0.5:
-        return 1
-    return 0
-
-
-multi_label_id = set()
-subgroup2cnt = defaultdict(lambda: defaultdict(int))
-for i in range(100000):
-    row = train_df.loc[i]
-    extra_row = extra_df.loc[i]
-    r_id = row['id']
-    e_id = extra_row['id']
-    assert r_id == e_id
-    target = pos2label(row['target'])
+def row2envs_id(ex_row: pd.Series):
+    envs_id = []
     for g, subs in group2subgroups.items():
         found = 0
-        for sub in subs[1:]:
-            if pd.notna(extra_row[sub]) and pos2label(extra_row[sub]):
+        for ind, sub in enumerate(subs[1:]):
+            if pd.notna(ex_row[sub]) and ex_row[sub] > 0.5:
                 found += 1
-                subgroup2cnt[sub][target] += 1
+                envs_id.append(1 + ind)
+                break
         if not found:
-            subgroup2cnt[subs[0]][target] += 1
-        if found > 1:
-            multi_label_id.add(r_id)
+            envs_id.append(0)
+    return envs_id
 
-for g, subs in group2subgroups.items():
-    sub2total = {}
-    sub2pos_ratio = {}
-    for sub in subs:
-        total = sum(subgroup2cnt[sub].values())
-        sub2total[sub] = total
-        sub2pos_ratio[sub] = subgroup2cnt[sub][1] / (total + 1e-9)
-    total = sum(sub2total.values())
-    sub2ratio = {k: v / total for k, v in sub2total.items()}
-    sub2pos_total_ratio = {}
-    for sub in subs:
-        sub2pos_total_ratio[sub] = sub2ratio[sub] * sub2pos_ratio[sub]
 
-    # keys = [k for k in sub2ratio if '[' not in k]
-    keys = [k for k in sub2ratio]
-    index = np.asarray(list(range(len(keys))))
-    ratio = [sub2ratio[keys[i]] for i in index]
-    local_pos_ration = [sub2pos_ratio[keys[i]] for i in index]
-    pos_ratio = [sub2pos_total_ratio[keys[i]] for i in index]
-    fig = plt.figure(figsize=(4, 8))
-    print(keys)
-    print(sub2ratio)
-    print(sub2pos_ratio)
-    print(sub2pos_total_ratio)
+if __name__ == '__main__':
+    def valid_pos(p):
+        if p > 0.5:
+            return 1
+        return 0
 
-    # filter the [no_XXX] subgroup away from showing ratio
-    # plt.bar(x=2 * index[1:], height=ratio[1:], width=0.2, color='yellow', label='ratio')
-    # plt.bar(x=2 * index, height=pos_ratio, width=0.2, color='green', label='pos_ratio')
-    plt.bar(x=2 * index + 1, height=local_pos_ration, width=0.2, color='blue', label='local_pos_ratio')
-    plt.xticks(2 * index + 0.5, keys, rotation=15)
-    plt.show()
-    _ = input()
+
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from collections import defaultdict
+
+    subgroup2group = {}
+    for g, subs in group2subgroups.items():
+        for sub in subs:
+            subgroup2group[sub] = g
+
+    t_path = r"train.csv"
+    e_path = r"train_extra.csv"
+    train_df = pd.read_csv(t_path)
+    extra_df = pd.read_csv(e_path)
+
+    multi_label_id = set()
+    subgroup2cnt = defaultdict(lambda: defaultdict(int))
+    for i in range(100000):
+        row = train_df.loc[i]
+        extra_row = extra_df.loc[i]
+        r_id = row['id']
+        e_id = extra_row['id']
+        assert r_id == e_id
+        target = valid_pos(row['target'])
+        for g, subs in group2subgroups.items():
+            found = 0
+            for sub in subs[1:]:
+                if pd.notna(extra_row[sub]) and valid_pos(extra_row[sub]):
+                    found += 1
+                    subgroup2cnt[sub][target] += 1
+            if not found:
+                subgroup2cnt[subs[0]][target] += 1
+            if found > 1:
+                multi_label_id.add(r_id)
+
+    for g, subs in group2subgroups.items():
+        sub2total = {}
+        sub2pos_ratio = {}
+        for sub in subs:
+            total = sum(subgroup2cnt[sub].values())
+            sub2total[sub] = total
+            sub2pos_ratio[sub] = subgroup2cnt[sub][1] / (total + 1e-9)
+        total = sum(sub2total.values())
+        sub2ratio = {k: v / total for k, v in sub2total.items()}
+        sub2pos_total_ratio = {}
+        for sub in subs:
+            sub2pos_total_ratio[sub] = sub2ratio[sub] * sub2pos_ratio[sub]
+
+        # keys = [k for k in sub2ratio if '[' not in k]
+        keys = [k for k in sub2ratio]
+        index = np.asarray(list(range(len(keys))))
+        ratio = [sub2ratio[keys[i]] for i in index]
+        local_pos_ration = [sub2pos_ratio[keys[i]] for i in index]
+        pos_ratio = [sub2pos_total_ratio[keys[i]] for i in index]
+        fig = plt.figure(figsize=(4, 8))
+        print(keys)
+        print(sub2ratio)
+        print(sub2pos_ratio)
+        print(sub2pos_total_ratio)
+
+        # filter the [no_XXX] subgroup away from showing ratio
+        # plt.bar(x=2 * index[1:], height=ratio[1:], width=0.2, color='yellow', label='ratio')
+        # plt.bar(x=2 * index, height=pos_ratio, width=0.2, color='green', label='pos_ratio')
+        plt.title(g)
+        plt.bar(x=2 * index + 1, height=local_pos_ration, width=0.2, color='blue', label='local_pos_ratio')
+        plt.xticks(2 * index + 0.5, keys, rotation=15)
+        plt.show()
+        _ = input()
--- data/split_data.py
+++ data/split_data.py
@@ -3,23 +3,46 @@ import random
 import numpy as np
 import pandas as pd
 
+group2subgroups = {
+    "disability": ["[no_disability]", "physical_disability", "psychiatric_or_mental_illness", "intellectual_or_learning_disability",
+                   "other_disability"],
+    "gender": ["[no_gender]", "male", "female", "transgender", "other_gender"],
+    "sexual_orientation": ["[no_sexual]", "homosexual_gay_or_lesbian", "heterosexual", "bisexual", "other_sexual_orientation"],
+    "race_or_ethnicity": ["[no_race]", "black", "white", "asian", "latino", "jewish", "other_race_or_ethnicity"],
+    "religion": ["[no_religion]", "christian", "buddhist", "atheist", "muslim", "hindu", "other_religion"],
+    # "misc": ["disagree", "sad", "funny", "wow", "likes"],
+}
+
+
+def row2envs_id(ex_row: pd.Series):
+    envs_id = []
+    for g, subs in group2subgroups.items():
+        found = 0
+        for ind, sub in enumerate(subs[1:]):
+            if pd.notna(ex_row[sub]) and ex_row[sub] > 0.5:
+                found += 1
+                envs_id.append(1 + ind)
+                break
+        if not found:
+            envs_id.append(0)
+    return envs_id
 
 if __name__ == '__main__':
     train_df = pd.read_csv('train.csv')
     extra_df = pd.read_csv('train_extra.csv')
     dev_ratio = 0.1
     length_dict = dict()
-    attrs = [
-        'male',
-        'female',
-        'homosexual_gay_or_lesbian',
-        'christian',
-        'jewish',
-        'muslim',
-        'black',
-        'white',
-        'psychiatric_or_mental_illness'
-    ]
+    # attrs = [
+    #     'male',
+    #     'female',
+    #     'homosexual_gay_or_lesbian',
+    #     'christian',
+    #     'jewish',
+    #     'muslim',
+    #     'black',
+    #     'white',
+    #     'psychiatric_or_mental_illness'
+    # ]
     dataset = list()
     for i in range(20000):
         row = train_df.loc[i]
@@ -27,15 +50,16 @@ if __name__ == '__main__':
         data = {
             'id': int(row['id']),
             'text': row['comment_text'],
-            'target': row['target']
+            'target': row['target'],
+            'env': row2envs_id(extra_row),
         }
         length = len(row['comment_text'].strip().split())
         if length in length_dict:
             length_dict[length] += 1
         else:
             length_dict[length] = 1
-        for attr in attrs:
-            data[attr] = extra_row[attr] if not np.isnan(extra_row[attr]) else -1
+        # for attr in attrs:
+        #     data[attr] = extra_row[attr] if not np.isnan(extra_row[attr]) else -1
         # data['identity_annotator_count'] = int(extra_row['identity_annotator_count'])
         dataset.append(data)
     random.shuffle(dataset)
